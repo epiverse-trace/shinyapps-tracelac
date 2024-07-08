c6709d1f7462bf5a90a1de79cddba6c234d5d0c4
Ernesto Perez <ec.perez@uniandes.edu.co>
2024-07-08 10:16:03 -0500

    Url for shiny apps

--- docs/colopendata/colopendata.js
+++ docs/colopendata/colopendata.js
@@ -1,17 +1,17 @@
 function changeIframeSrc(newSrc) {
     // Create a switch to change the iframe source
     switch (newSrc) {
-        case 'catalogo-descriptivo':
-            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/epico/';
+        case 'datasets':
+            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/datasets/';
             break;
         case 'divipola':
             document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/divipola/';
             break;
         case 'demograficos':
-            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/epico/';
+            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/demograficos/';
             break;
         case 'geoespaciales':
-            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/epico/';
+            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/geoespaciales/';
             break;
         case 'clima':
             document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/climate/'            ;
