52f26bc3e34703c390c4e89efc7ceb0751c3cb61
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2024-07-09 16:11:07 -0500

    Add new url for shiny

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,2 @@
-# Base R Shiny image
-FROM rocker/shiny
-
-# Make a directory in the container
-RUN mkdir /home/shiny-app
-RUN apt-get update && apt-get install libudunits2-dev r-cran-devtools -y
-
-# Install R dependencies
-RUN R -e "install.packages(c('dplyr', 'ggplot2', 'gapminder','shinyjs','shinycssloaders'))"
-RUN R -e "install.packages('remotes')"
-RUN R -e "remotes::install_github('epiverse-trace/epiCo')"
-
-# Copy the Shiny app code
-COPY shiny-apps/epico/app.R /home/shiny-app/epico/app.R
-
-# Expose the application port
-EXPOSE 8180
-
-# Run the R Shiny app
-#CMD ["R", "-e", "shiny::runApp('/home/shiny-app/epico/app.R')"]
-CMD ["R", "-e", "library(shiny); source('/home/shiny-app/epico/app.R')"]
+FROM httpd:2.4
+COPY ./docs /usr/local/apache2/htdocs/
--- docs/colopendata/colopendata.js
+++ docs/colopendata/colopendata.js
@@ -2,22 +2,22 @@ function changeIframeSrc(newSrc) {
     // Create a switch to change the iframe source
     switch (newSrc) {
         case 'datasets':
-            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/datasets/';
+            document.getElementById('myIframe').src = 'https://tracelac.uniandes.edu.co:3838/shiny-apps/colopendata/climate';
             break;
         case 'divipola':
-            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/divipola/';
+            document.getElementById('myIframe').src = 'https://tracelac.uniandes.edu.co:3838/shiny-apps/colopendata/divipola';
             break;
         case 'demograficos':
-            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/demograficos/';
+            document.getElementById('myIframe').src = 'https://tracelac.uniandes.edu.co:3838/shiny-apps/colopendata/download-demographic';
             break;
         case 'geoespaciales':
-            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/geoespaciales/';
+            document.getElementById('myIframe').src = 'https://tracelac.uniandes.edu.co:3838/shiny-apps/colopendata/download-geospatial';
             break;
         case 'clima':
-            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/climate/'            ;
+            document.getElementById('myIframe').src = 'https://tracelac.uniandes.edu.co:3838/shiny-apps/colopendata/list-datasets';
             break;
         case 'proypoblacionales':
-            document.getElementById('myIframe').src = 'https://tracelac.shinyapps.io/population-projections/';
+            document.getElementById('myIframe').src = 'https://tracelac.uniandes.edu.co:3838/shiny-apps/colopendata/population-projections';
             break;
     }
 }
