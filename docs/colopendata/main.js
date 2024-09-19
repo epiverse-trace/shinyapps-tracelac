5e22fd15f258d23ec1c6fc258c1abcd3e9f8f088
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2024-09-19 10:27:47 -0500

    Remove unused objects

--- docs/colopendata/main.js
+++ docs/colopendata/main.js
@@ -1,3 +1,13 @@
+document.getElementById('spinner').style.display = 'none';
+
 function submit() {
   alert("Datos registrados correctamente.");
+}
+
+function showSpinner() {
+  document.getElementById('spinner').style.display = 'block';
+  
+}
+function hideSpinner() {
+  document.getElementById('spinner').style.display = 'none';
 }
