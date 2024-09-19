b5896f0f6ee2b95263094f047e06ceab717adddd
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2024-09-19 10:53:22 -0500

    Remove unused objects

--- docs/colopendata/main.js
+++ docs/colopendata/main.js
@@ -8,6 +8,7 @@ function showSpinner() {
   document.getElementById('spinner').style.display = 'block';
   
 }
+
 function hideSpinner() {
   document.getElementById('spinner').style.display = 'none';
 }
